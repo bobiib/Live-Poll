c847af2a4fa1cf703185e26cf0599eb5201146a2
Benjamin Gerresheim <benjaminmikagerresheim@gmail.com>
2026-03-19 11:43:23 +0100

    - switched from gradle to maven

--- backend-src/src/main/java/com/livepoll/backendsrc/BackendSrcApplication.java
+++ backend-src/src/main/java/com/livepoll/backendsrc/BackendSrcApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendSrcApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BackendSrcApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSrcApplication.class, args);
+	}
 
 }
--- backend-src/src/test/java/com/livepoll/backendsrc/BackendSrcApplicationTests.java
+++ backend-src/src/test/java/com/livepoll/backendsrc/BackendSrcApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BackendSrcApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
