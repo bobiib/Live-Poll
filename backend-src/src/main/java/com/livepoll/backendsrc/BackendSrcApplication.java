8bc140f336624bea7e98ecf17960ed14be753b19
Benjamin Gerresheim <benjaminmikagerresheim@gmail.com>
2026-03-19 11:21:33 +0100

    - added initial backend-src setup

---
+++ backend-src/src/main/java/com/livepoll/backendsrc/BackendSrcApplication.java
@@ -0,0 +1,13 @@
+package com.livepoll.backendsrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSrcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSrcApplication.class, args);
+    }
+
+}
